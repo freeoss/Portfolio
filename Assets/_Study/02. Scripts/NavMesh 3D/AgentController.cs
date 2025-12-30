e362d02e3f508ef69ada6af4a9a9bb500a0b3f13
freeoss <freeoss@naver.com>
2025-12-30 11:53:57 +0900

    NavMesh ing

--- Assets/_Study/02. Scripts/NavMesh 3D/AgentController.cs
+++ Assets/_Study/02. Scripts/NavMesh 3D/AgentController.cs
@@ -1,18 +1,40 @@
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AgentController : MonoBehaviour
 {
     private NavMeshAgent agent;
-    public Transform destination;
+    public NavMeshSurface surface;
+
+    private float bakeDistance = 10f;
     
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        
+        surface. transform.position = transform.position;
+        surface.BuildNavMesh();
     }
 
     void Update()
     {
-        agent.SetDestination(destination.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        float dist = Vector3.Distance(transform.position, surface.transform.position);
+        if (dist > bakeDistance)
+        {
+            surface. transform.position = transform.position;
+            surface.BuildNavMesh();
+        }
     }
 }
