3938b451b85f5a2c6f56a44ad5a6c8f55bda8d7a
freeoss <freeoss@naver.com>
2026-01-05 14:39:40 +0900

    Update CommitTest.cs

--- Assets/_Study/02. Scripts/CommitTest.cs
+++ Assets/_Study/02. Scripts/CommitTest.cs
@@ -5,7 +5,7 @@ public class CommitTest : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Hello Github");
     }
 
     // Update is called once per frame
